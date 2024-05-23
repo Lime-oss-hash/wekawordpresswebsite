e16b72703d6e7457ced3157597f70ec83b808883
Lime-oss-hash <dedicationpop@outlook.com>
2024-05-24 11:11:08 +1200

    123
    commit

---
+++ web/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:",8VUwE|9{gEGVAcRA/])4v9RUGq(gW/E5g$ogkFc-yhy93Fghc&& !lGM?e<:xb0";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1617;}
